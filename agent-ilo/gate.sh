1ae9e9ae0a39b7153b2bac2b7726707723b9ce13
ankit-kumar5 <ankit.kumar5@hpe.com>
2018-01-26 07:13:14 -0500

    qq

--- agent-ilo/gate.sh
+++ agent-ilo/gate.sh
@@ -88,6 +88,7 @@ function update_ironic {
     git config --global user.email "proliantutils@gmail.com"
     git config --global user.name "proliantci"
     git fetch https://git.openstack.org/openstack/ironic refs/changes/51/535651/1 && git cherry-pick FETCH_HEAD
+    git fetch https://git.openstack.org/openstack/ironic refs/changes/25/454625/18 && git cherry-pick FETCH_HEAD
 }
 
 function update_ironic_tempest_plugin {
