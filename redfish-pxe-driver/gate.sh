968dae7bcd3d6745ae604b0ab8758c8ffbb42d26
proliantci <proliantutils@gmail.com>
2019-09-30 12:53:03 +0530

    Adding redfish-pxe gate

--- redfish-pxe-driver/gate.sh
+++ redfish-pxe-driver/gate.sh
@@ -25,6 +25,14 @@ export PATH=$PATH:/var/lib/gems/1.8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin
 export DIB_DEPLOY_ISO_KERNEL_CMDLINE_ARGS="console=ttyS1"
 export IRONIC_USER_IMAGE_PREFERRED_DISTRO=${IRONIC_USER_IMAGE_PREFERRED_DISTRO:-fedora}
 export BOOT_LOADER=${BOOT_LOADER:-grub2}
+export no_proxy=169.16.1.54
+wget http://169.16.1.54:9999/proxy -P /home/ubuntu/
+source /home/ubuntu/proxy
+sudo chmod 0600 /home/ubuntu/zuul_id_rsa
+wget http://169.16.1.54:9999/log_upload_ssh -P /home/ubuntu/
+wget http://169.16.1.54:9999/config -P /home/ubuntu/.ssh/
+sudo chmod 0600 /home/ubuntu/log_upload_ssh
+sudo chmod 0664 /home/ubuntu/.ssh/config
 
 function install_packages {
     sudo apt -y install apache2
@@ -35,10 +43,7 @@ function install_packages {
     sudo pip install setuptools
     sudo pip3 install proliantutils
     sudo chown ubuntu.ubuntu /var/www/html
-    #wget http://mirror.mtl01.inap.openstack.org/wheel/ubuntu-18.04-x86_64/kombu/kombu-4.2.2-py2.py3-none-any.whl
-    #sudo pip3 install kombu-4.2.2-py2.py3-none-any.whl
     sudo chmod 600 /home/ubuntu/zuul_id_rsa
-    sudo chmod 600 /home/ubuntu/log_server_id_rsa
 }
 
 function clone_projects {
@@ -52,7 +57,7 @@ function clone_projects {
 }
 
 function configure_dhcp_server {
-    wget http://10.13.120.214:9999/redfish_pxe_dhcp_server.txt -P /opt/stack/devstack/files/
+    wget http://169.16.1.54:9999/redfish_pxe_dhcp_server.txt -P /opt/stack/devstack/files/
     ip=$(ip addr show ens3 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
     mac=$(cat /tmp/hardware_info |cut -f2 -d ' ')
     sed -i "s/8.8.8.8/$ip/g" /opt/stack/devstack/files/redfish_pxe_dhcp_server.txt
@@ -63,10 +68,9 @@ function configure_dhcp_server {
 
 function configure_interface {
     ip1=$(ip addr show ens3 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-    sudo ip route add 10.0.0.0/8 via 10.13.120.193 dev ens3
     sudo modprobe 8021q
     sudo vconfig add ens3 100
-    sudo ifconfig ens3.100 inet $ip1 netmask 255.255.255.224
+    sudo ifconfig ens3.100 inet $ip1 netmask 255.255.255.0
 }
 
 function run_stack {
@@ -75,16 +79,14 @@ function run_stack {
     local capabilities
 
     cd /opt/stack/devstack/files
-    wget http://10.13.120.214:9999/cirros-0.3.5-x86_64-uec.tar.gz
-    wget http://10.13.120.214:9999/cirros-0.3.5-x86_64-disk.img
-    wget http://10.13.120.214:9999/ir-deploy-pxe_ilo.initramfs
-    wget http://10.13.120.214:9999/ir-deploy-pxe_ilo.kernel
-    wget http://10.13.120.214:9999/ubuntu-uefi.img
-    wget http://10.13.120.214:9999/grubx64.efi
-    wget http://10.13.120.214:9999/bootx64.efi
-    wget http://10.13.120.214:9999/shim.efi
-    wget http://10.13.120.214:9999/ipxe.efi
-    cp ubuntu-uefi.img ir-deploy-pxe_ilo.kernel ir-deploy-pxe_ilo.initramfs cirros-0.3.5-x86_64-disk.img cirros-0.3.5-x86_64-uec.tar.gz /var/www/html
+    wget http://169.16.1.54:9999/ir-deploy-pxe_ilo.initramfs
+    wget http://169.16.1.54:9999/ir-deploy-pxe_ilo.kernel
+    wget http://169.16.1.54:9999/ubuntu-uefi.img
+    wget http://169.16.1.54:9999/grubx64.efi
+    wget http://169.16.1.54:9999/bootx64.efi
+    wget http://169.16.1.54:9999/shim.efi
+    wget http://169.16.1.54:9999/ipxe.efi
+    cp ubuntu-uefi.img ir-deploy-pxe_ilo.kernel ir-deploy-pxe_ilo.initramfs /var/www/html
     # Add new line character in hardware_info so it will readable
     sed -i 's|ironmantesting|ironmantesting /redfish/v1/Systems/1|' /tmp/hardware_info
     echo  >> /tmp/hardware_info
@@ -93,6 +95,7 @@ function run_stack {
     cp /tmp/redfish-pxe-driver/HPE-CI-JOBS/redfish-pxe-driver/local.conf.sample local.conf
     ip=$(ip addr show ens3 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
     sed -i "s/192.168.1.2/$ip/g" local.conf
+
     # Run stack.sh
     ./stack.sh
     cp /opt/stack/devstack/files/ipxe.efi /opt/stack/data/ironic/tftpboot/
@@ -100,18 +103,27 @@ function run_stack {
     sudo sed -i "s/pxe_grub_config.template/ipxe_config.template/g" /etc/ironic/ironic.conf
     sudo systemctl restart devstack@ir-api
     sudo systemctl restart devstack@ir-cond
-    
-    # Modify the node to reflect the boot_mode and secure_boot capabilities.
-    # Also modify the nova flavor accordingly.
-    sudo ovs-vsctl del-br br-ens3.100
-    source /opt/stack/devstack/openrc admin admin
-    ironic_node=$(ironic node-list | grep -v UUID | grep "\w" | awk '{print $2}' | tail -n1)
-    #ironic node-update $ironic_node add driver_info/ilo_deploy_iso=http://10.13.120.214:9999/fedora-raid-deploy-ank-proliant-tools.iso
-    ironic node-update $ironic_node add driver_info/deploy_kernel=http://10.13.120.214:9999/ir-deploy-pxe_ilo.kernel driver_info/deploy_ramdisk=http://10.13.120.214:9999/ir-deploy-pxe_ilo.initramfs 
-    ironic node-update $ironic_node add instance_info/image_source=http://10.13.120.214:9999/ubuntu-uefi.img instance_info/image_checksum=a46f6297446f1197510839ef70d667c5
-    ironic node-update $ironic_node add instance_info/capabilities='{"boot_option": "local"}'
 
-    ironic node-set-power-state $ironic_node off
+    #Reaccess to private network
+    sudo ovs-vsctl del-br br-ens3
+    sudo ip link set ens3 down
+    sudo ip link set ens3 up
+
+    #Create Node
+    source /opt/stack/devstack/openrc admin admin
+    ilo_ip=$(cat /tmp/hardware_info | awk '{print $1}')
+    mac=$(cat /tmp/hardware_info | awk '{print $2}')
+
+    openstack baremetal node create --driver ilo --driver-info ilo_address=$ilo_ip --driver-info ilo_username=Administrator --driver-info ilo_password=weg0th@ce@r --driver-info console_port=5000
+
+    ironic_node=$(openstack baremetal node list | grep -v UUID | grep "\w" | awk '{print $2}' | tail -n1)
+
+    openstack baremetal node manage $ironic_node
+    openstack baremetal node provide $ironic_node
+    openstack baremetal node set --driver-info deploy_kernel=http://169.16.1.54:9999/ir-deploy-pxe_ilo.kernel --driver-info deploy_ramdisk=http://169.16.1.54:9999/ir-deploy-pxe_ilo.initramfs --instance-info image_source=http://169.16.1.54:9999/ubuntu-uefi.img --instance-info image_checksum=a46f6297446f1197510839ef70d667c5 --instance-info capabilities='{"boot_mode": "uefi"}' --instance-info capabilities='{"boot_option": "local"}' --property capabilities='boot_mode:uefi' $ironic_node
+
+    openstack baremetal port create --node $ironic_node $mac
+    openstack baremetal node power off $ironic_node
 
     # Run the tempest test.
     cd /opt/stack/tempest
@@ -123,8 +135,7 @@ function update_ironic {
     cd /opt/stack/ironic
     git config --global user.email "proliantutils@gmail.com"
     git config --global user.name "proliantci"
-    #git fetch https://git.openstack.org/openstack/ironic refs/changes/25/454625/18 && git cherry-pick FETCH_HEAD
-    git fetch https://review.opendev.org/openstack/ironic refs/changes/25/454625/18 && git cherry-pick FETCH_HEAD
+    git fetch https://review.opendev.org/openstack/ironic refs/changes/25/454625/19 && git cherry-pick FETCH_HEAD
 }
 
 function update_ironic_tempest_plugin {
