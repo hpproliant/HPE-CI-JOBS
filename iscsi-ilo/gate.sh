14d0b6fbe14f0d40813e09e425a4e776ff967319
proliantci <proliantutils@gmail.com>
2020-09-07 11:51:30 +0530

    Adding verify_ca attribute to node creation for ilo gates

--- iscsi-ilo/gate.sh
+++ iscsi-ilo/gate.sh
@@ -20,7 +20,7 @@ ip=$(ip addr show ens2 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 export OS_AUTH_TYPE=none
 export OS_ENDPOINT=http://$ip/baremetal
 
-openstack baremetal node create --driver ilo --driver-info ilo_address=$ilo_ip --driver-info ilo_username=Administrator --driver-info ilo_password=weg0th@ce@r --driver-info console_port=5000
+openstack baremetal node create --driver ilo --driver-info ilo_address=$ilo_ip --driver-info ilo_username=Administrator --driver-info ilo_password=weg0th@ce@r --driver-info console_port=5000 --driver-info ilo_verify_ca=False
 
 ironic_node=$(openstack baremetal node list | grep -v UUID | grep "\w" | awk '{print $2}' | tail -n1)
 
