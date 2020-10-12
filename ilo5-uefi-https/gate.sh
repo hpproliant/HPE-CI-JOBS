8bc17eff9665d18a332c04f512d437c0ad152447
proliantci <proliantutils@gmail.com>
2020-10-12 11:24:37 +0530

    Adding UEFI HTTPS Gate

---
+++ ilo5-uefi-https/gate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+source /home/ubuntu/proxy
+
+# Configure dhcp server
+wget http://169.16.1.54:9999/uefi_https_dhcp_server.txt -P /opt/stack/devstack/files/
+mac=$(cat /tmp/hardware_info | awk '{print $2}')
+#mac=70:10:6f:b4:cd:b6
+sed -i "s/8c:dc:d4:af:78:ec/$mac/g" /opt/stack/devstack/files/uefi_https_dhcp_server.txt
+sudo sh -c 'cat /opt/stack/devstack/files/uefi_https_dhcp_server.txt >> /etc/dhcp/dhcpd.conf'
+sudo service isc-dhcp-server restart
+
+#Create Node
+#source /opt/stack/devstack/openrc admin admin
+ilo_ip=$(cat /tmp/hardware_info | awk '{print $1}')
+mac=$(cat /tmp/hardware_info | awk '{print $2}')
+#ilo_ip=169.16.1.14
+#mac=70:10:6f:b4:cd:b6
+
+unset OS_REGION_NAME OS_PROJECT_DOMAIN_ID OS_AUTH_URL OS_TENANT_NAME OS_USER_DOMAIN_ID OS_USERNAME OS_VOLUME_API_VERSION OS_AUTH_TYPE OS_PROJECT_NAME OS_PASSWORD OS_IDENTITY_API_VERSION
+
+ip=$(ip addr show ens2 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+export OS_AUTH_TYPE=none
+export OS_ENDPOINT=http://$ip/baremetal
+
+openstack baremetal node create --driver ilo --driver-info ilo_address=$ilo_ip --driver-info ilo_username=Administrator --driver-info ilo_password=weg0th@ce@r --driver-info console_port=5000 --driver-info ilo_verify_ca=False
+
+ironic_node=$(openstack baremetal node list | grep -v UUID | grep "\w" | awk '{print $2}' | tail -n1)
+
+openstack baremetal node manage $ironic_node
+openstack baremetal node provide $ironic_node
+openstack baremetal node set --driver-info ilo_deploy_iso=http://169.16.1.54:9999/fedora_04_06_20.iso --instance-info image_source=http://169.16.1.54:9999/fedora-wd-uefi.img --instance-info image_checksum=17a6c6df66d4c90b05554cdc2285d851 --instance-info capabilities='{"boot_mode": "uefi"}' --property capabilities='boot_mode:uefi' $ironic_node
+
+openstack baremetal port create --node $ironic_node $mac
+openstack baremetal node power off $ironic_node
+
+# Run the tempest test.
+cd /opt/stack/tempest
+export OS_TEST_TIMEOUT=3000
+sudo tox -e all -- ironic_standalone.test_basic_ops.BaremetalIloDirectWholediskHttpLink.test_ip_access_to_server
---
+++ ilo5-uefi-https/prepare_gate.sh
@@ -0,0 +1,103 @@
+#!/bin/bash -x
+
+# Gate script for running tempest tests with iLO drivers in different
+# configurations.  The following environment variables are expected:
+#
+# ILO_HWINFO_GEN8_SYSTEM - Hardware info for Gen8 system.
+# ILO_HWINFO_GEN9_SYSTEM - Hardware info for Gen9 system.
+# IRONIC_ELILO_EFI - Absolute path of elilo.efi file
+# IRONIC_FEDORA_SHIM - Absolute path of fedora signed shim.efi
+# IRONIC_FEDORA_GRUBX64 - Absolute path of fedora signed grubx64.efi
+# IRONIC_UBUNTU_SHIM - Absolute path of ubuntu signed shim.efi
+# IRONIC_UBUNTU_GRUBX64 - Absolute path of grub signed grubx64.efi
+# http_proxy - Proxy settings
+# https_proxy - Proxy settings
+# HTTP_PROXY - Proxy settings
+# HTTPS_PROXY - Proxy settings
+# no_proxy - Proxy settings
+
+env
+
+set -e
+set -x
+set -o pipefail
+echo "We are inside agent gate"
+export PATH=$PATH:/var/lib/gems/1.8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export DIB_DEPLOY_ISO_KERNEL_CMDLINE_ARGS="console=ttyS1"
+export IRONIC_USER_IMAGE_PREFERRED_DISTRO=${IRONIC_USER_IMAGE_PREFERRED_DISTRO:-fedora}
+export BOOT_OPTION=${BOOT_OPTION:-}
+export SECURE_BOOT=${SECURE_BOOT:-}
+export BOOT_LOADER=${BOOT_LOADER:-grub2}
+export IRONIC_IPA_RAMDISK_DISTRO=ubuntu
+export BRANCH=${ZUUL_BRANCH:-master}
+source /home/ubuntu/proxy
+
+function install_packages {
+    sudo apt -y update
+    sudo apt -y install apache2
+    sudo apt -y install python-pip
+    sudo apt -y install python3-pip
+    sudo apt -y install python3-setuptools
+    sudo apt -y install isc-dhcp-server
+    sudo apt -y install webfs socat vlan liberasurecode-dev libssl-dev virtualenv nginx
+    sudo pip3 install proliantutils
+}
+
+function configure_interface {
+   # ip1=$(ip addr show ens2 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+   # sudo sh -c 'echo web_root='/opt/stack/devstack/files' >> /etc/webfsd.conf'
+   # sudo sh -c 'echo web_ip='$ip1' >> /etc/webfsd.conf'
+   # sudo sh -c 'echo web_port=8010 >> /etc/webfsd.conf'
+    sudo cp /tmp/HPE-CI-JOBS/ilo5-uefi-https/conf/* /etc/nginx/conf.d/
+    sudo service nginx restart
+}
+
+function create_cert {
+    mkdir /home/ubuntu/ssl_files
+    ip=$(ip addr show ens2 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+    #Creating the certificate
+    sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /home/ubuntu/ssl_files/uefi_signed.key -out /home/ubuntu/ssl_files/uefi_signed.crt -subj "/C=IN/ST=KCN=$ip"
+}
+
+function run_stack {
+
+    local ironic_node
+    local capabilities
+
+    cd /opt/stack/devstack
+    wget http://169.16.1.54:9999/ir-deploy-ilo.iso -P files/
+    wget http://169.16.1.54:9999/fedora-wd-uefi.img -P files/
+    echo  >> /tmp/hardware_info
+    cp /tmp/agent-ilo/HPE-CI-JOBS/agent-ilo/local.conf.sample local.conf
+    ip=$(ip addr show ens2 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+    sed -i "s/192.168.1.2/$ip/g" local.conf
+
+    ./stack.sh
+
+    sleep 30
+
+    #Reaccess to private network
+    sudo ovs-vsctl del-br br-ens2
+    sudo ip link set ens2 down
+    sudo ip link set ens2 up
+}
+
+function update_ironic {
+    cd /opt/stack/ironic
+    git config --global user.email "proliantutils@gmail.com"
+    git config --global user.name "proliantci"
+}
+
+function update_ironic_tempest_plugin {
+    cd /opt/stack/ironic-tempest-plugin
+    #git fetch https://git.openstack.org/openstack/ironic-tempest-plugin refs/changes/52/535652/11 && git cherry-pick FETCH_HEAD
+    sudo python3 setup.py install
+}
+
+install_packages
+configure_interface
+create_cert
+#update_ironic
+#update_ironic_tempest_plugin
+run_stack
